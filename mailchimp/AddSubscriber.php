f441df66fa9d1ba1a6812a61c968e75d7d442b28
datingsite <jbh@northstar-productions.com>
2014-09-18 00:03:16 -0700

    fixes for AddSubscriber.php

--- mailchimp/AddSubscriber.php
+++ mailchimp/AddSubscriber.php
@@ -1,22 +1,20 @@
 <?php
 include_once 'MailChimp.php';
 
-print_r($POST);
-
-if(isset($POST['email'])) {
-    if(!isset($POST['fname'])) {
-        $POST['fname'] = "";
+if(isset($_POST['email'])) {
+    if(!isset($_POST['fname'])) {
+        $_POST['fname'] = "";
     }
 
-    if(!isset($POST['message'])) {
-        $POST['message'] = "";
+    if(!isset($_POST['message'])) {
+        $_POST['message'] = "";
     }
 
     $MailChimp = new \Drewm\MailChimp('71713c5c25ae5cff901283697882ea43-us9');
     $result = $MailChimp->call('lists/subscribe', array(
         'id'                => 'd04a8dfa0e',
-        'email'             => array('email' => $POST['email']),
-        'merge_vars'        => array('FNAME' => $POST['fname'], 'MESSAGE' => $POST['message']),
+        'email'             => array('email' => $_POST['email']),
+        'merge_vars'        => array('FNAME' => $_POST['fname'], 'MESSAGE' => $_POST['message']),
         'double_optin'      => false,
         'update_existing'   => true,
         'replace_interests' => false,
